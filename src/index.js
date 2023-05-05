787cdb2144736596da29433564cea192994b8c48
Maksim <maksim.saraev.121.5@gmail.com>
2023-05-05 20:47:20 +0300

    video and lesson styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './index.html';
 
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import './index.scss';
+
+const videoBG = document.querySelector('.video-bg');
+// video.style.cssText = `
+//     border: 20px solid red;
+// `;
+videoBG.innerHTML = `
+<source src="video/video.webm" type="video/webm">
+<source src="video/video.mp4" type="video/mp4">
+`;
